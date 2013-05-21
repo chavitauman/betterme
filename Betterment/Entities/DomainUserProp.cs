41558d22eb6de3f24fbadccdda97f9cff8f5e350
pesitauman <pesitauman@gmail.com>
2013-05-21 08:09:22 +0300

    add comment

--- Betterment/Entities/DomainUserProp.cs
+++ Betterment/Entities/DomainUserProp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
-
+//223
 namespace Entities
 {
     [Table("T_DOMAIN_USERS_PROP")]
